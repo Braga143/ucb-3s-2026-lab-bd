3cfc3aeb8e8c5df8d697d0df5c04b7e547083a84
WALISSON ALVES BRAGA <walissonalvesbraga@gmail.com>
2026-03-04 21:38:24 -0300

    aula12026

---
+++ aula 1/criar_usuario1s2026.sql
@@ -0,0 +1,12 @@
+USE biblioteca_pessoal_1S2026;
+
+create table Usuario (
+id_usuario int auto_increment primary key,
+nome varchar(255) not null,
+email varchar(255) not null unique,
+data_cadastro date not null
+);
+
+show tables;
+
+
